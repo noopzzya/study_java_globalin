cec4d52d5db3c1d0c81b53e9979c5a11847f22fc
jia <noopzzya@gmail.com>
2022-11-28 17:30:44 +0900

    221128

--- Day07/src/com/dowhileex/DowhileEx02.java
+++ Day07/src/com/dowhileex/DowhileEx02.java
@@ -3,9 +3,20 @@ package com.dowhileex;
 public class DowhileEx02 {
 
 	public static void main(String[] args) {
-		int i = 1, sum=0;
-		
-		
+		int i = 1;
+		int sum = 0;
+					
+		do {
+			sum += i;
+			if(i<10) {
+			System.out.print(i + " + ");
+			} else {
+					System.out.print(i + " = ");
+					System.out.print(sum);
+				}
+				i++;
+			} while (i <= 10);
+				
 	}
 
 }
