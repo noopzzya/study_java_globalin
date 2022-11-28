298d6b119f4c8a6248374d224585d3c9c28c6d2a
jia <noopzzya@gmail.com>
2022-11-28 16:22:42 +0900

    221128

---
+++ Day13/src/com/absex/InterFaceEx.java
@@ -0,0 +1,17 @@
+package com.absex;
+
+public class InterFaceEx implements InterEx {
+
+	@Override
+	public int getA() {
+		
+		return A;
+	}
+
+	public static void main(String[] args) {
+		InterFaceEx ie = new InterFaceEx();
+		System.out.println(ie.getA());
+	}
+	
+	
+}
--- Test/src/study/MainEx.java
+++ Test/src/study/MainEx.java
@@ -2,12 +2,8 @@ package study;
 
 public class MainEx{ 
 
-	private int a = 5;
-	protected int b = 10;
-	public int c = 20;
-
-	public void write() {
-		System.out.println("SuperEx write() 메소드:" +a+ ":" +b+ ":" +c);
+	public static void main(String[] args) {
+		
 	}
-	//test
+
 }
