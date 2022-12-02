2aa485d7dca0d1d9e54c78409b977090b52246bb
jia <noopzzya@gmail.com>
2022-12-02 11:45:45 +0900

    1202

---
+++ Day14/src/com/enumex/Color.java
@@ -0,0 +1,8 @@
+package com.enumex;
+
+public enum Color {
+
+	Red, Green, Blue, Skyblue
+	
+	
+}
--- Test/src/study/Test4.java
+++ Test/src/study/Test4.java
@@ -3,8 +3,7 @@ package study;
 public class Test4 {
 
 	public static void main(String[] args) {
-		
-	
+			
 		/*
 		int arr[][] = new int[5][4];
 		int n=1;
